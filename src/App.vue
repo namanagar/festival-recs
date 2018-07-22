6ae87269af01b893e0e110d7bb2f373d30b6317e
Naman Agarwal <niffleheim22@gmail.com>
2018-07-22 06:03:17 -0500

    auth working, loading gif created

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,48 @@
   <div id="app">
     <div class="row">
       <div class="col-sm-12">
-        <h2>who2see</h2>
-        <p>You're super excited to go to {{ textFeature }} - the headliners are sick. But who are you gonna watch earlier in the day 
-          when the lineup is full of names you've never heard of? Your options are to listen to a ton of music (& not dig a lot of it) before the festival 
+        <h2><span style="color: #fb4f4f">who</span><span style="color: #6cc0e5">2</span><span style="color: #fbc93d">see</span></h2>
+        <p>You're excited to see some of your favorite bands at {{ textFeature }}. But who are you gonna watch earlier in the day 
+          when the lineup is full of names you've never heard of? Your options are to do a ton of research before the festival 
           to find the artists you do want to see, or just mill around on the day of. This app aims to help with that: we take your Spotify listening history,
           analyze it, and recommend sets to watch.
         </p>
       </div>
     </div>
-   <div class="btn-group btn-group-lg btn-group-toggle" role="group" aria-label="festivals" v-for="festival in festivals" :key="festival.name">
-     <label class="btn" :class="[(fest == festival) ? 'btn-success' : 'btn-outline-success']">
-        <input type="radio" name="options" id="option1" autocomplete="off" :value="festival.name" v-model="fest"> {{ festival.name }}
-      </label>
-   </div>
+    <div class="row padded">
+      <div class="col-sm-12">
+        <div class="btn-group btn-group-lg btn-group-toggle" role="group" aria-label="festivals" v-for="myFestival in festivals" :key="myFestival.name">
+          <label class="btn" :class="[(fest == myFestival) ? 'btn-primary' : 'btn-outline-primary']">
+            <input type="radio" name="options" id="option1" autocomplete="off" :value="myFestival.name" v-model="fest"> {{ myFestival.name }}
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+          <small class="center">Check back for more festivals - Gov Ball, Coachella, Bonnaroo, etc.!</small>
+      </div>
+    </div>
+    <div  v-if="this.user == null" class="row extra-padded">
+      <div class="col-sm-12">
+          <button class="btn btn-lg btn-success" @click="login()"><i class="fab fa-spotify"/>  Log in with Spotify</button>
+      </div>
+    </div>
+    <div v-if="this.user != null" class="row extra-padded">
+      <div class="col-sm-12">
+          <div class="card" style="width: 100%;">
+            <div class="card-body">
+              <h5 class="card-title">{{user.display_name}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{user.email}}</h6>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div v-if="this.user != null && loading == true" class="row">
+      <div class="col-sm-12">
+          <img src="src/assets/loading.gif"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +52,7 @@
 //import axios to make the http requests to the spotify api
 var spotify = require("spotify-web-api-js");
 var firebase = require("firebase");
+var axios = require("axios");
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyCDvQPlAs_uSfVwNHdA27WwJeIFHN2rXIY",
@@ -38,7 +68,10 @@ export default {
   name: "app",
   data() {
     return {
-      fest: ""
+      fest: "Lollapalooza 2018",
+      _token: "",
+      user: null,
+      loading: false
     };
   },
   firebase: function() {
@@ -50,6 +83,75 @@ export default {
     textFeature(){
       return this.fest == "" ? "a music festival" : this.fest;
     }
+  },
+  methods: {
+    async login() {
+      /**************
+       *** AUTH *****
+       **************
+       this code is taken (very slightly modified) from https://glitch.com/edit/#!/spotify-implicit-grant?path=README.md:1:0
+       which explains how to follow the spotify implicit grant authorization flow
+      */
+      const hash = window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce(function(initial, item) {
+          if (item) {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+          }
+          return initial;
+        }, {});
+      //window.location.hash = "";
+      // Set token
+      this._token = hash.access_token;
+      const authEndpoint = "https://accounts.spotify.com/authorize";
+      // Replace with your app's client ID, redirect URI and desired scopes
+      const clientId = "47d9e6f3d4364d13bc1a0572ed81a078";
+      const redirectUri = "http://localhost:8080/";
+      const scopes = [
+        "user-top-read user-read-private user-read-email"
+      ];
+      // If there is no token, redirect to Spotify authorization
+      if (!this._token) {
+        window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+          "%20"
+        )}&response_type=token&show_dialog=true`;
+      }
+    }
+  },
+  created: function(){
+    const hash = window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce(function(initial, item) {
+          if (item) {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+          }
+          return initial;
+        }, {});
+      this._token = hash.access_token;
+      const idk = hash.access_token;
+      var retVal = false;
+      axios
+        .get("https://api.spotify.com/v1/me", {
+          headers: {
+            Authorization: "Bearer " + this._token
+          }
+        })
+        .then(
+          response => {
+            this.user = response.data;
+            this._token = idk;
+            retVal = true;
+          },
+          error => {
+            console.log("error: " + error);
+            retVal = false;
+          }
+        );
+        return retVal;
   }
 };
 </script>
@@ -61,7 +163,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #f4f4f4;
+  color: #2d2d2d;
   margin-left: 7.5vmax;
   margin-right: 7.5vmax;
   margin-top: 5vmax;
@@ -88,5 +190,14 @@ a {
 
 .fest-button {
   margin: 1em;
+}
+.center {
+  text-align: center;
+}
+.padded{
+  margin-top: 0.75em;
+}
+.extra-padded{
+  margin-top: 3em;
 }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
-
+var babelPolyfill = require("babel-polyfill")
 module.exports = {
-  entry: './src/main.js',
+  entry: ['babel-polyfill', './src/main.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
