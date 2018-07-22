c0cd2293ff8efcf10cb5eea9950d8d2f9cbef65d
Naman Agarwal <niffleheim22@gmail.com>
2018-07-22 12:03:58 -0500

    trying gh-pages, recommendations working

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@
       <div class="col-sm-12">
           <small class="center">Check back for more festivals - Gov Ball, Coachella, Bonnaroo, etc.!</small>
       </div>
+      <button @click="generate()">generate</button>
     </div>
     <div  v-if="this.user == null" class="row extra-padded">
       <div class="col-sm-12">
@@ -51,10 +52,28 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-4" v-for="artist in guarantees" :key="artist.id">
+        <div class="col-sm-4" v-for="artist in guarantees" :key="artist.artist">
+          <div class="card" style="width: 100%;">
+            <div class="card-body">
+              <h5 class="card-title">{{artist.artist}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{ formatStartDate(artist.startTime)}} - {{formatEndDate(artist.endTime)}}</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+     <div v-if="this.user != null && loading == false && recommendations.length > 0">
+      <div class="row extra-padded">
+        <div class="col-sm-12">
+          <h4>Recommendations:</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4" v-for="artist in recommendations" :key="artist.name">
           <div class="card" style="width: 100%;">
             <div class="card-body">
               <h5 class="card-title">{{artist.name}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{ formatStartDate(artist.startTime)}} - {{formatEndDate(artist.endTime)}}</h6>
             </div>
           </div>
         </div>
@@ -89,18 +108,39 @@ export default {
       user: null,
       loading: false,
       topArtists: [],
-      guarantees: []
+      guarantees: [],
+      relatedArtists: [],
+      freqMap: [],
+      recommendations: []
     };
   },
   firebase: {
-      festivals: festivalsRef
+    festivals: festivalsRef
   },
   computed: {
-    textFeature(){
+    textFeature() {
       return this.fest == "" ? "a music festival" : this.fest;
     }
   },
   methods: {
+    formatStartDate(date) {
+      var obj = new Date(date);
+      obj.setTime( obj.getTime() - new Date().getTimezoneOffset()*60*1000 );
+      return (
+        obj.toDateString().substring(0, 3) +
+        " " +
+        (obj.toLocaleTimeString("en-US").length < 11
+          ? obj.toLocaleTimeString("en-US").substring(0, 4)
+          : obj.toLocaleTimeString("en-US").substring(0, 5))
+      );
+    },
+    formatEndDate(date) {
+      var obj = new Date(date);
+      obj.setTime( obj.getTime() - new Date().getTimezoneOffset()*60*1000 );
+      return obj.toLocaleTimeString("en-US").length < 11
+        ? obj.toLocaleTimeString("en-US").substring(0, 4)
+        : obj.toLocaleTimeString("en-US").substring(0, 5);
+    },
     async login() {
       /* this code is taken (very slightly modified) from https://glitch.com/edit/#!/spotify-implicit-grant?path=README.md:1:0
        which explains how to follow the spotify implicit grant authorization flow */
@@ -120,10 +160,8 @@ export default {
       const authEndpoint = "https://accounts.spotify.com/authorize";
       // Replace with your app's client ID, redirect URI and desired scopes
       const clientId = "47d9e6f3d4364d13bc1a0572ed81a078";
-      const redirectUri = "http://localhost:8080/";
-      const scopes = [
-        "user-top-read user-read-private user-read-email"
-      ];
+      const redirectUri = "https://namanagar.github.io/festival-recs/"; // http://localhost:8080/
+      const scopes = ["user-top-read user-read-private user-read-email"];
       // If there is no token, redirect to Spotify authorization
       if (!this._token) {
         window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
@@ -132,99 +170,129 @@ export default {
       }
     },
     async getTopArtists() {
-        await axios
-          .get(
-            `https://api.spotify.com/v1/me/top/artists?limit=50`,
-            {
-              headers: {
-                Authorization: "Bearer " + this._token
-              }
-            }
-          )
-          .then(
-            response => {
-              response.data.items.forEach(element => {
-                this.topArtists.push({ id: element.id, name: element.name })
-              });
-            },
-            error => {
-              console.log("error: " + error);
-            }
-          );
-    },
-    findGuarantees() {
-      var myFest = this.fest
-      var lineup = '';
-      this.festivals.forEach(fest => {
-        if (fest.name == myFest){
-          lineup = fest.lineup
-        }
-      })
-      var lineupNames = lineup.map(el => el.artist);
-      var matches = this.topArtists.filter(artist => {
-        return lineupNames.includes(artist.name);
-      })
-      return matches;
-    },
-    async getRelatedArtists(artistId){
       await axios
-          .get(
-            `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
-            {
-              headers: {
-                Authorization: "Bearer " + this._token
-              }
-            }
-          )
-          .then(
-            response => {
-              console.log(response.data)
-              var related = [];
-              response.data.artists.forEach(element => {
-                related.push({ id: element.id, name: element.name });
-              });
-              return related;
-            },
-            error => {
-              console.log("error: " + error);
-            }
-          );
-    },
-    async generate(){
-      this.loading = true;
-      await this.getTopArtists();
-      this.guarantees = this.findGuarantees();
-      this.loading = false;
-    }
-  },
-  created: function(){
-    const hash = window.location.hash
-        .substring(1)
-        .split("&")
-        .reduce(function(initial, item) {
-          if (item) {
-            var parts = item.split("=");
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-          }
-          return initial;
-        }, {});
-      this._token = hash.access_token;
-      const idk = hash.access_token;
-      axios
-        .get("https://api.spotify.com/v1/me", {
+        .get(`https://api.spotify.com/v1/me/top/artists?limit=50`, {
           headers: {
             Authorization: "Bearer " + this._token
           }
         })
         .then(
           response => {
-            this.user = response.data;
-            this._token = idk;
+            response.data.items.forEach(element => {
+              this.topArtists.push({ id: element.id, name: element.name });
+            });
           },
           error => {
             console.log("error: " + error);
           }
         );
+    },
+    findGuarantees() {
+      var myFest = this.fest;
+      var lineup = "";
+      this.festivals.forEach(fest => {
+        if (fest.name == myFest) {
+          lineup = fest.lineup;
+        }
+      });
+      var artistNames = this.topArtists.map(el => el.name);
+      var matches = lineup.filter(artist => {
+        return artistNames.includes(artist.artist);
+      });
+      return matches;
+    },
+    async getRelatedArtists(topArtists) {
+      var artistIDs = topArtists.map(el => el.id);
+      for (var i = 0; i < artistIDs.length; i++) {
+        await axios
+          .get(
+            `https://api.spotify.com/v1/artists/${
+              artistIDs[i]
+            }/related-artists`,
+            {
+              headers: {
+                Authorization: "Bearer " + this._token
+              }
+            }
+          )
+          .then(
+            response => {
+              //console.log(response.data);
+              response.data.artists.forEach(element => {
+                this.relatedArtists.push(element.name);
+              });
+            },
+            error => {
+              console.log("error: " + error);
+            }
+          );
+      }
+      return this.relatedArtists;
+    },
+    buildMap() {
+      var myFest = this.fest;
+      var lineup = "";
+      this.festivals.forEach(fest => {
+        if (fest.name == myFest) {
+          lineup = fest.lineup;
+        }
+      });
+      var lineupArtists = lineup.map(el => el.artist.toUpperCase());
+      var guarantees = this.guarantees.map(el => el.artist.toUpperCase());
+      this.relatedArtists.forEach(rel => {
+        if (lineupArtists.includes(rel.toUpperCase()) && !guarantees.includes(rel.toUpperCase())) {
+          var justNames = this.freqMap.map(el => el.name.toUpperCase())
+          if (!justNames.includes(rel.toUpperCase())) {
+            var obj = lineup.find(p => p.artist.toUpperCase() == rel.toUpperCase())
+            this.freqMap.push({name: rel, count: 1, startTime: obj.startTime, endTime: obj.endTime});
+          }
+          else {
+            let artist = this.freqMap.find(p => p.name == rel)
+            artist.count += 1;
+          }
+        }
+      });
+      var byCount = this.freqMap.slice(0);
+      byCount.sort(function(a,b) { return a.count - b.count; });
+      this.recommendations = byCount;
+    },
+    async generate() {
+      this.loading = true;
+      await this.getTopArtists();
+      this.guarantees = this.findGuarantees();
+      await this.getRelatedArtists(this.topArtists);
+      this.buildMap();
+      this.loading = false;
+    }
+  },
+  created: function() {
+    const hash = window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce(function(initial, item) {
+        if (item) {
+          var parts = item.split("=");
+          initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+      }, {});
+    this._token = hash.access_token;
+    const idk = hash.access_token;
+    axios
+      .get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + this._token
+        }
+      })
+      .then(
+        response => {
+          this.user = response.data;
+          this._token = idk;
+        },
+        error => {
+          console.log("error: " + error);
+        }
+      );
   }
 };
 </script>
@@ -240,6 +308,7 @@ export default {
   margin-left: 7.5vmax;
   margin-right: 7.5vmax;
   margin-top: 5vmax;
+  margin-bottom: 5vmax;
 }
 h1,
 h2 {
@@ -267,10 +336,10 @@ a {
 .center {
   text-align: center;
 }
-.padded{
+.padded {
   margin-top: 0.75em;
 }
-.extra-padded{
+.extra-padded {
   margin-top: 3em;
 }
 </style>
